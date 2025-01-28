c9c9c7d45880b0bd02a6a24ad5239918ff028e20
scalderon10 <s.calderon10@uniandes.edu.co>
2025-01-27 23:45:38 -0500

    Nueva función para cargar datos SCL

--- app.py
+++ app.py
@@ -21,7 +21,9 @@ app.config.suppress_callback_exceptions = True
 
 # Load data from csv
 def load_data():
-    # To do: Completar la función 
+    data = pd.read_csv('datos_energia.csv', parse_dates=['time'])
+    data = data.set_index('time')
+    return data
     
 
 # Cargar datos
